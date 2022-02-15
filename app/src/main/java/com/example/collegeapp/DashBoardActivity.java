7b4cc6dc17c4e663eb086b135b230a6e9102d0b8
sadasivanks <sadasadasivan2000@gmail.com>
2022-02-15 15:10:53 +0530

    college app balance

--- app/src/main/java/com/example/collegeapp/DashBoardActivity.java
+++ app/src/main/java/com/example/collegeapp/DashBoardActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class DashBoardActivity extends AppCompatActivity {
 AppCompatButton b1,b2,b3,b4,b5,b6;
+SharedPreferences mypref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +22,16 @@ AppCompatButton b1,b2,b3,b4,b5,b6;
         b4=(AppCompatButton) findViewById(R.id.sef);
         b5=(AppCompatButton) findViewById(R.id.vweb);
         b6=(AppCompatButton) findViewById(R.id.lg);
+        mypref=getSharedPreferences("login",MODE_PRIVATE);
+
         b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                SharedPreferences.Editor myedit=mypref.edit();
+                myedit.clear();
+                myedit.commit();
+
                 Intent i=new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(i);
             }
--- app/src/main/java/com/example/collegeapp/MainActivity.java
+++ app/src/main/java/com/example/collegeapp/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,16 +14,27 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 EditText ed1,ed2;
 AppCompatButton b1,b2;
-String getemail,getpass;
+String getemail,getpass,prefvalue;
+SharedPreferences mypref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mypref=getSharedPreferences("login", MODE_PRIVATE);
         ed1=(EditText) findViewById(R.id.eid);
         ed2=(EditText) findViewById(R.id.pass);
         b1=(AppCompatButton) findViewById(R.id.log);
         b2=(AppCompatButton) findViewById(R.id.gtr);
+
+        prefvalue=mypref.getString("email",null);
+        if(prefvalue!=null)
+        {
+            Intent i=new Intent(getApplicationContext(),DashBoardActivity.class);
+            startActivity(i);
+        }
+
+
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +49,12 @@ String getemail,getpass;
                 getpass=ed2.getText().toString();
                 if (getemail.equals("admin")&&getpass.equals("12345"))
                 {
+                    SharedPreferences.Editor myedit=mypref.edit();
+                    myedit.putString("email",getemail);
+
+
+                    myedit.commit();
+
                     Intent i=new Intent(getApplicationContext(),DashBoardActivity.class);
                     startActivity(i);
                 }
